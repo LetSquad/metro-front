1712cb39830bb58fbf892c74fc56fa71ee719a36
ivansavoskin <vanyasamail@gmail.com>
2024-06-22 15:00:16 +0300

    Исправлена загрузка приложения

--- src/components/App/AppContent.tsx
+++ src/components/App/AppContent.tsx
@@ -5,16 +5,18 @@ import { WithSuspense } from "@coreUtils/WithSuspense";
 import Routes from "@pages/Routes";
 import { selectIsCurrentEmployeeLoading } from "@store/employee/selectors";
 import { useAppSelector } from "@store/hooks";
+import { selectIsEmployeeNotAuth } from "@store/info/selectors";
 
 import styles from "./styles/AppContent.module.scss";
 
 export default function AppContent() {
     const isEmployeeLoading = useAppSelector(selectIsCurrentEmployeeLoading);
+    const isNotAuth = useAppSelector(selectIsEmployeeNotAuth);
 
     return (
         <div className={styles.container}>
             <div className={styles.mainContentWrapper}>
-                {isEmployeeLoading ? (
+                {isEmployeeLoading && !isNotAuth ? (
                     <div className={partsStyles.flexBaseCenterContainer}>
                         <Loader active inline="centered" />
                     </div>
